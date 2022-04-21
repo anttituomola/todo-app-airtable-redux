ceefcaccc80f52e9e36d4f6202b8a239e9ab5530
Antti <anttituomola8@gmail.com>
2022-04-21 11:27:15 +0300

    initial redux slice

--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -3,24 +3,13 @@ import { createSlice } from "@reduxjs/toolkit"
 export const counterSlice = createSlice({
     name: "counter",
 
-    // Initial state on pakollinen, jotta Redux saa jotain dataa sovelluksen latautuessa ensimmäistä kertaa.
     initialState: {
-        value: 0,
-        incrementValue: 0
+        todos: [],
     },
 
-    /* Tässä käytetään mutatoivaa eli suoraan arvoa muuttavaa metodia (state.value += 1)
-    Se on mahdollista ja sallittua ainoastaan, kun käytämme Toolkitin createSlice-metodia,
-    jonka asiosta datan muuttumattomuus saavutetaan kulissien takana. */
     reducers: {
-        increment: state => {
-            state.value += 1
-        },
-        decrement: state => {
-            state.value -= 1
-        },
-        incrementByAmount: (state, action) => {
-            state.incrementValue = parseInt(action.payload)
+        hydrateTodos: (state, action) => {
+            state.todos = action.payload
         }
     }
 })
