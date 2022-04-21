33cbeb7948dd921a038308e81caf3d72ceb47e32
Antti <anttituomola8@gmail.com>
2022-04-21 11:39:25 +0300

    hydrate redux

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import './App.css'
 import { FiltersEl } from './components/FiltersEl'
 import { InputEl } from './components/InputEl'
 import { TodosEl } from './components/TodosEl'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { hydrateTodos } from './features/todoSlice'
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+      fetch("https://api.airtable.com/v0/appI5bp3TfUEMBRDH/todos?api_key=keyMx6L7Z9LjE5XF8")
+        .then(response => response.json())
+        .then(data => {
+        dispatch(hydrateTodos(data.records))
+        })
+  }, [dispatch])
+
   return (
     <div className="App">
       <h1>Todo app with Airtable backend and Redux state</h1>
