4eaef28d50c6e216e223248bb0fe587476a00939
Antti <anttituomola8@gmail.com>
2022-04-22 11:10:01 +0300

    items left feature

--- src/components/FiltersEl.js
+++ src/components/FiltersEl.js
@@ -1,13 +1,15 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { filterMode } from "../features/todoSlice"
 
 export const FiltersEl = () => {
     const dispatch = useDispatch()
+    const todos = useSelector(state => state.todos.todos)
+    const itemsLeft = todos.filter(todo => !todo.fields.done).length
 
     return (
         <div id="filters">
             <div>
-                <p>Items left: </p>
+                <p>Items left: {itemsLeft}</p>
             </div>
             <div id="filterButtons">
                 <button className="filterButton" onClick={() => dispatch(filterMode("all"))}>All</button>
