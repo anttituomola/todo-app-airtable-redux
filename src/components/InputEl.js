3e969a4810dfb43bd0cb017b0c96f5360229b1fb
Antti <anttituomola8@gmail.com>
2022-04-22 11:13:21 +0300

    prevent empty todos

--- src/components/InputEl.js
+++ src/components/InputEl.js
@@ -1,27 +1,29 @@
 import { useState } from "react"
 import { useDispatch } from "react-redux"
-import {addTodoThunk} from "../backendSync/addTodoThunk";
+import { addTodoThunk } from "../backendSync/addTodoThunk";
 
 export const InputEl = () => {
-    const [value, setValue] = useState("")
-    const dispatch = useDispatch()
+  const [value, setValue] = useState("")
+  const dispatch = useDispatch()
 
-    const handleSubmit = () => {
+  const handleSubmit = () => {
+    if (value) {
       const todo = {
-          fields: {
-              todoText: value,
-              done: false,
-          },
-          id: Date.now(),
+        fields: {
+          todoText: value,
+          done: false,
+        },
+        id: Date.now(),
       }
       setValue("")
       dispatch(addTodoThunk(todo))
     }
+  }
 
   return (
     <div className="inputEl">
-        <input className="inputField" value={value} type="text" placeholder="Add todo" onChange={(event) => setValue(event.target.value)}/>
-        <button className="inputButton" onClick={handleSubmit}>Add</button>
+      <input className="inputField" value={value} type="text" placeholder="Add todo" onChange={(event) => setValue(event.target.value)} />
+      <button className="inputButton" onClick={handleSubmit}>Add</button>
     </div>
   )
 }
