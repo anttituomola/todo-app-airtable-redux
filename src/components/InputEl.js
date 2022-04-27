81c13187d0735ac742fca1b00f32bb7e22a95c9c
Antti <anttituomola8@gmail.com>
2022-04-27 14:25:01 +0300

    autofocus

--- src/components/InputEl.js
+++ src/components/InputEl.js
@@ -1,10 +1,17 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { addTodoThunk } from "../backendSync/addTodoThunk";
 
 export const InputEl = () => {
+  const inputField = useRef(null)
   const [value, setValue] = useState("")
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (inputField.current) {
+      inputField.current.focus()
+    }
+  }, [])
 
   const listenForEnter = (e) => {
     console.log("KEY")
@@ -26,9 +33,18 @@ export const InputEl = () => {
     }
   }
 
+  // There's a bug: when we're submitting a new todo, it get's dispatched to Airtable, and in there, an ID will be created.
+  // This ID is now, however, update immidiately back to Redux state, so when we try to delete or edit it, it will fail to update the data in Airtable.
+  // Possible solution: fetch updated data from Airtable, and update Redux state with it after every dispatch?
+
   return (
     <div className="inputEl">
-      <input className="inputField" value={value} type="text" placeholder="Add todo" onChange={(event) => setValue(event.target.value)} onKeyDown={listenForEnter}/>
+      <input className="inputField" 
+        ref={inputField}
+        value={value} type="text" 
+        placeholder="Add todo" 
+        onChange={(event) => setValue(event.target.value)} 
+        onKeyDown={listenForEnter}/>
       <button className="inputButton" onClick={handleSubmit}>Add</button>
     </div>
   )
